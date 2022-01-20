b7bad206346cbf107ec4d89c548daa0fa31fe745
Valentin Poirot <valentin.poirot@outlook.com>
2022-01-20 14:39:05 +0100

    c ok

--- connect-4-main/model.js
+++ connect-4-main/model.js
@@ -1,6 +1,6 @@
 class Model {
   constructor() {
-    this.player = 0;
+    this.player = 1;
     this.matrixW = 7;
     this.matrixH = 6;
     this.matrix = this.initializeMatrix();
@@ -41,76 +41,77 @@ class Model {
   initializeMatrix() {
     this.matrix = [];
     let row = [];
-    for (let i = 0; i < this.matrixW; i++) {
+    for(let i = 0 ; i<this.matrixW ; i++){
       row = [];
-      for (let j; j < this.matrixH; j++) {
+      for (let j = 0; j<this.matrixH ; j++){
         row.push(0);
       }
+      this.matrix.push(row);
     }
   };
 
-  setMatrix(columns, rows) {
-    this.matrix[columns][rows] = this.player;
-  };
+    setMatrix(columns, rows) {
+        this.matrix[columns][rows] = this.player;
+    };
 
-  changePlayer() {
-    if (this.player === 0) {
-      this.player = 1;
-    } else {
-      this.player = 0;
+    changePlayer(){
+      if (this.player === 0){
+        this.player = 1;
+      }else {
+        this.player = 0;
+      }
     }
-  }
 
-  checkWin() {
-    //check horizontal
-    for (let x = 0; x < this.matrixW - 3; x++) {
-      for (let y = 0; y < this.matrixH; y++) {
-        if (this.matrix[x][y] === this.player && this.matrix[x + 1][y] === this.player && this.matrix[x + 2][y] === this.player && this.matrix[x + 3][y] === this.player) {
-          return 1;
+    checkWin(){
+      //check horizontal
+      for (let x = 0 ; x < this.matrixW - 3 ; x++){
+        for (let y = 0 ; y < this.matrixH ; y++){
+          if ( this.matrix[x][y] === this.player && this.matrix[x+1][y] === this.player && this.matrix[x+2][y] === this.player && this.matrix[x+3][y] === this.player){
+            return 1;
+          }
+        }
+      }
+
+      //check vertical
+      for (let x = 0 ; x < this.matrixW ; x++){
+        for (let y = 0 ; y < this.matrixH -3 ; y++){
+          if ( this.matrix[x][y] === this.player && this.matrix[x][y+1] === this.player && this.matrix[x][y+2] === this.player && this.matrix[x][y+3] === this.player){
+            return 1;
+          }
+        }
+      }
+
+      //check diagonal bas gauche haut droit
+      for (let x = 0 ; x < this.matrixW - 3 ; x++){
+        for (let y = 0 ; y < this.matrixH - 3 ; y++){
+          if ( this.matrix[x][y] === this.player && this.matrix[x+1][y+1] === this.player && this.matrix[x+2][y+2] === this.player && this.matrix[x+3][y+3] === this.player){
+            return 1;
+          }
+        }
+      }
+
+      //check diagonal
+      for (let x = 3; x < this.matrixW ; x++){
+        for (let y = 0 ; y < this.matrixH - 3 ; y++){
+          if ( this.matrix[x][y] === this.player && this.matrix[x-1][y+1] === this.player && this.matrix[x-2][y-2] === this.player && this.matrix[x-3][-3] === this.player){
+            return 1;
+          }
         }
       }
     }
 
-    //check vertical
-    for (let x = 0; x < this.matrixW; x++) {
-      for (let y = 0; y < this.matrixH - 3; y++) {
-        if (this.matrix[x][y] === this.player && this.matrix[x][y + 1] === this.player && this.matrix[x][y + 2] === this.player && this.matrix[x][y + 3] === this.player) {
+    addToken(column){
+      let i = 0;
+      do {
+        if (this.matrix[i][column] === 0){
+          this.setMatrix(i,column);
+          return 0;
+        }else if (i === 7){
           return 1;
+        } else{
+          i++
         }
-      }
-    }
-
-    //check diagonal bas gauche haut droit
-    for (let x = 0; x < this.matrixW - 3; x++) {
-      for (let y = 0; y < this.matrixH - 3; y++) {
-        if (this.matrix[x][y] === this.player && this.matrix[x + 1][y + 1] === this.player && this.matrix[x + 2][y + 2] === this.player && this.matrix[x + 3][y + 3] === this.player) {
-          return 1;
-        }
-      }
-    }
-
-    //check diagonal
-    for (let x = 3; x < this.matrixW; x++) {
-      for (let y = 0; y < this.matrixH - 3; y++) {
-        if (this.matrix[x][y] === this.player && this.matrix[x - 1][y + 1] === this.player && this.matrix[x - 2][y - 2] === this.player && this.matrix[x - 3][-3] === this.player) {
-          return 1;
-        }
-      }
-    }
-  }
-
-  addToken(column) {
-    let i = 0;
-    do {
-      if (this.matrix[column][i] === 0) {
-        this.setMatrix(column, i);
-        return 0;
-      } else if (i === 7) {
-        return 1;
-      } else {
-        i++
-      }
-    } while (1);
+      }while (1);
 
   }
 }
